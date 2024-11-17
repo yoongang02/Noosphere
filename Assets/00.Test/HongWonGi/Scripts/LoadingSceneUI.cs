ddf43fa956502d0d3c6f5bd6d55631abaf0bec81
Hone Won Gi <keyone957@naver.com>
2024-11-17 18:16:36 +0900

    Fix : Dotween 사용해서 fade 인 아웃

--- Assets/00.Test/HongWonGi/Scripts/LoadingSceneUI.cs
+++ Assets/00.Test/HongWonGi/Scripts/LoadingSceneUI.cs
@@ -4,12 +4,13 @@ using TMPro;
 using UnityEngine.SceneManagement;
 using System;
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
 public class LoadingSceneUI : MonoBehaviour
 {
-    [SerializeField] private Slider loadingSlider;
-    [SerializeField] private TextMeshProUGUI loadingText;
-    [SerializeField] private RawImage fadeImage;
-    [SerializeField] private float fadeDuration = 1f;
+    [SerializeField] private Slider _loadingSlider;
+    [SerializeField] private TextMeshProUGUI _loadingText;
+    [SerializeField] private RawImage _fadeImage;
+    [SerializeField] private float _fadeDuration = 0.8f;
     
     private void Start()
     {
@@ -29,39 +30,14 @@ public class LoadingSceneUI : MonoBehaviour
 
     private void UpdateLoadingProgress(float progress)
     {
-        loadingSlider.value = progress;
-        loadingText.text = $"로딩중 : {progress * 100f}%";
+        _loadingSlider.value = progress;
+        _loadingText.text = $"로딩중 : {progress * 100f}%";
     }
 
-    private async UniTask FadeIn()
-    {
-        float elapsed = 0f;
-        Color color = fadeImage.color;
-        color.a = 1f;
-        fadeImage.color = color;
-
-        while (elapsed < fadeDuration)
-        {
-            elapsed += Time.deltaTime;
-            color.a = Mathf.Lerp(1f, 0f, elapsed / fadeDuration);
-            fadeImage.color = color;
-            await UniTask.Yield();
-        }
-    }
-
+   
     private async UniTask FadeOut()
     {
-        float elapsed = 0f;
-        Color color = fadeImage.color;
-        color.a = 0f;
-        fadeImage.color = color;
-
-        while (elapsed < fadeDuration)
-        {
-            elapsed += Time.deltaTime;
-            color.a = Mathf.Lerp(0f, 1f, elapsed / fadeDuration);
-            fadeImage.color = color;
-            await UniTask.Yield();
-        }
+        _fadeImage.color = new Color(0, 0, 0, 0);
+        await _fadeImage.DOFade(1, _fadeDuration).AsyncWaitForCompletion();
     }
 }
