af9933cd7ce24c474bc96f627e21db9aef857507
Hone Won Gi <keyone957@naver.com>
2024-11-22 01:56:23 +0900

    Feat : SoundManager 배치

--- Assets/02.Scripts/Manager/Define.cs
+++ Assets/02.Scripts/Manager/Define.cs
@@ -3,5 +3,5 @@ public static class Define
     public const string _bgmRoot = "BGM";
     public const string _soundRoot = "Sound";
 
-    // public const string soundresource_001 = "soundresource_001";
+    public const string soundresource_001 = "soundresource_001";
 }
