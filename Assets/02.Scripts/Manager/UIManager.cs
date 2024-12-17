3bad4837dd5a85225f405275a94f7bb9511debe0
YoonKyoungMin <96673951+yoongang02@users.noreply.github.com>
2024-12-17 17:45:08 +0900

    Feat : Event_A010 까지 연결
    - dialogue ui 또한 UIManger에서 열고 닫기 관리.
    
    - UIManager에서 UI를 오픈하면 기본적으로 플레이어의 행동(이동,상호작용) 금지
    
    - 모든 UI가 닫혀야 플레이어가 행동 재개 가능

--- Assets/02.Scripts/Manager/UIManager.cs
+++ Assets/02.Scripts/Manager/UIManager.cs
@@ -14,9 +14,9 @@ public class UIManager : Singleton<UIManager>
     public UIBase investigateUI;
     public UIBase evidenceDetailUI;
     public UIBase inventoryUI;
-    public GameObject inventoryIcon;
+    public UIBase dialogueUI;
     
-    public TextMeshProUGUI dialogueUI;
+    public GameObject inventoryIcon;
     
     public bool isInMap = true;
     
@@ -32,6 +32,7 @@ public class UIManager : Singleton<UIManager>
         // ESC 버튼 입력 처리
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            if(IsUIOpen(dialogueUI)) return;
             CloseTopUI();
         }
 
@@ -51,6 +52,7 @@ public class UIManager : Singleton<UIManager>
 
         // 스택에 추가하고 UI를 활성화
         // 상호작용 금지
+        LockPlayer();
         uiStack.Push(ui);
         ui.OnOpen();
     }
@@ -66,6 +68,7 @@ public class UIManager : Singleton<UIManager>
         }
         // 스택에 추가하고 UI를 활성화
         // 상호작용 금지
+        LockPlayer();
         uiStack.Push(ui);
         ui.OnOpen(evidence);
     }
@@ -75,11 +78,13 @@ public class UIManager : Singleton<UIManager>
         if (uiStack.Count == 0) return;
 
         UIBase topUI = uiStack.Pop();
+        
         topUI.OnClose();
 
         if (!IsAnyUIOpen())
         {
             //상호작용 금지 해제
+            UnLockPlayer();
         }
     }
     
@@ -110,5 +115,19 @@ public class UIManager : Singleton<UIManager>
     public bool IsAcquiredInInvestigateUI()
     {
         return investigateUI.GetComponent<InvestigateUI>().isAquired;
+    }
+    
+    public void LockPlayer()
+    {
+        Debug.Log("UI 열 때 LockPlayer 실행되나?");
+        PlayerController.Instance.canMove = false;
+        PlayerInteract.Instance.canInteract = false;
+    }
+
+    public void UnLockPlayer()
+    {
+        Debug.Log("UI 모두 닫히면 UnLockPlayer 실행되나?");
+        PlayerController.Instance.canMove = true;
+        PlayerInteract.Instance.canInteract = true;
     }
 }
