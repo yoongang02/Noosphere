94980d82f8cba574a289f8121edc3f1930498380
Hone Won Gi <keyone957@naver.com>
2025-01-13 20:55:40 +0900

    Fix : Effect_18 수정

--- Assets/00.Test/HongWonGi/Scripts/Effect/TimeLineController.cs
+++ Assets/00.Test/HongWonGi/Scripts/Effect/TimeLineController.cs
@@ -4,28 +4,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
+
 public class TimeLineController : MonoBehaviour
 {
-   [SerializeField] private PlayableDirector _playableDirector;
-   [SerializeField] private GameObject _playerParent;
-   private GameObject _player;
-   private void Awake()
-   {
-      _player = GameObject.FindObjectOfType<PlayerController>().gameObject;
-   }
-   private void OnEnable()
-   {
-      _player.transform.SetParent(_playerParent.transform);
-      _playableDirector.Play();
-   }
+    [SerializeField] private PlayableDirector _playableDirector;
+    [SerializeField] private GameObject _playerParent;
+    private GameObject _player;
 
-   private void Start()
-   {
-      _playableDirector.stopped += EndTimeLine;
-   }
-   private void EndTimeLine(PlayableDirector obj)
-   {
-      EffectManager.Instance.OnEffectEnd?.Invoke();
-      _player.transform.SetParent(null);
-   }
+    private void Awake()
+    {
+        _player = GameObject.FindObjectOfType<PlayerController>().gameObject;
+    }
+
+    private void OnEnable()
+    {
+        if (_playerParent != null)
+        {
+            _player.transform.SetParent(_playerParent.transform);
+            Vector3 newPosition = _player.transform.position;
+            newPosition.x = 0f;
+            newPosition.z = 0f;
+            _player.transform.localPosition = newPosition;
+        }
+
+        _playableDirector.Play();
+    }
+
+    private void Start()
+    {
+        _playableDirector.stopped += EndTimeLine;
+    }
+
+    private void EndTimeLine(PlayableDirector obj)
+    {
+        EffectManager.Instance.OnEffectEnd?.Invoke();
+        Debug.Log("타임라인 끝");
+
+        if (_playerParent != null)
+        {
+            _player.transform.SetParent(null);
+        }
+    }
 }
