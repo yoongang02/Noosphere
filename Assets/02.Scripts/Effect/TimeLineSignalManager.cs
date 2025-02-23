cf664f3ed95e5dedb8d421618538f38bb1f55c9f
Hone Won Gi <keyone957@naver.com>
2025-02-23 20:16:32 +0900

    Feat : 시그널 애니메이션 함수 추가
    - 애니메이션 함수 추가
    - 딕셔너리 사용하여 캐싱해서 사용.

--- Assets/02.Scripts/Effect/TimeLineSignalManager.cs
+++ Assets/02.Scripts/Effect/TimeLineSignalManager.cs
@@ -9,17 +9,20 @@ using UnityEngine.UI;
 using Cinemachine;
 
 public class TimeLineSignalManager : MonoBehaviour
-{ 
-    [Header("npc및 캐릭터 애니메이션 관련")]
-    [SerializeField] private Animator _npc;
+{
+    [Header("npc및 캐릭터 애니메이션 관련")] [SerializeField]
+    private Animator _npc;
+
     [SerializeField] private Animator _friendNpc;
     private Animator _player;
-  
+
     [SerializeField] private SkinnedMeshRenderer _freindMaterial;
     [SerializeField] private Material _friendMaterial;
     [SerializeField] private List<string> footSteps;
     [SerializeField] CinemachineVirtualCamera _farLoungCam;
-    private void Start() 
+    
+    private Dictionary<string, Animator> _animCacheDic = new Dictionary<string, Animator>();
+    private void Start()
     {
         _player = GameObject.Find("Player").GetComponent<Animator>();
         _mainCamera = Camera.main;
@@ -28,131 +31,167 @@ public class TimeLineSignalManager : MonoBehaviour
             _cameraData = _mainCamera.GetComponent<UnityEngine.Rendering.Universal.UniversalAdditionalCameraData>();
         }
     }
-    
+
+    #region 애니메이션 관련
+
     public void StartPlayerAnim(string playerAnim)
-   {
-       _player.SetBool(playerAnim,true);
-   }
+    {
+        _player.SetBool(playerAnim, true);
+    }
 
-   public void EndPlayerAnim(string playerAnim)
-   {
-       _player.SetBool(playerAnim,false);
-   }
-   
+    public void EndPlayerAnim(string playerAnim)
+    {
+        _player.SetBool(playerAnim, false);
+    }
 
-   public void StartNpcAnim(string npcAnim)
-   {
-       _npc.SetBool(npcAnim,true);
-   }
+    public void StartNpcAnim(string npcAnim)
+    {
+        _npc.SetBool(npcAnim, true);
+    }
 
-   public void EndNpcAnim(string npcAnim)
-   {
-       _npc.SetBool(npcAnim,false);
-   }
-   public void StartFriendNpcAnim(string npcAnim)
-   {
-       _friendNpc.SetBool(npcAnim,true);
-       _freindMaterial.material = _friendMaterial;
-   }
+    public void EndNpcAnim(string npcAnim)
+    {
+        _npc.SetBool(npcAnim, false);
+    }
 
-   public void EndFriendNpcAnim(string npcAnim)
-   {
-       _friendNpc.SetBool(npcAnim,false);
-   }
+    public void StartFriendNpcAnim(string npcAnim)
+    {
+        _friendNpc.SetBool(npcAnim, true);
+        _freindMaterial.material = _friendMaterial;
+    }
 
-   public void SetMaterial(Material material)
-   {
-       _freindMaterial.material = material;
-   }
+    public void EndFriendNpcAnim(string npcAnim)
+    {
+        _friendNpc.SetBool(npcAnim, false);
+    }
 
-   /// <summary>
-   /// ///////////////////////////////////////////////////
-   /// </summary>
-   [Header("정신세계 이펙트")]
-   [SerializeField] private RawImage _vhsImage;
-   [SerializeField] private Volume _vhsVolume;
-   [SerializeField] private GameObject _vhsObj;
-   [SerializeField] private float _duration = 5f;
-   private Camera _mainCamera;
-   private UnityEngine.Rendering.Universal.UniversalAdditionalCameraData _cameraData;
-   private async UniTaskVoid StartAutoEffect()
-   {
-       _vhsObj.SetActive(true);
-       _cameraData.renderPostProcessing = true;
+    #endregion
 
-       float elapsedTime = 0f;
-       SoundManager.Instance.PlaySFXNoEffect("Soundresource_029");
-       // 5초동안 게이지 증가
-       while (elapsedTime < _duration)
-       {
-           elapsedTime += Time.deltaTime;
-           float value = elapsedTime / _duration;
-           
-           Color color = _vhsImage.color;
-           color.a = value;
-           _vhsImage.color = color;
-           _vhsVolume.weight = value;
-      
-           await UniTask.Yield();
-       }
-       SoundManager.Instance.PlaySFXNoEffect("Soundresource_028");
-       DontDestroyOnLoad(_player.gameObject);
-   }
-   public void OnStartMentalProcess()
-   {
-       StartAutoEffect().Forget();
-   }
+    private Animator GetAnimator(string npcObjectName)
+    {
+        if (!_animCacheDic.ContainsKey(npcObjectName))
+        {
+            Animator anim = GameObject.Find(npcObjectName)?.GetComponent<Animator>();
+            _animCacheDic[npcObjectName] = anim;
+        }
+        return _animCacheDic[npcObjectName];
+    }
 
-   public void OnLoadScene(string SceneName)
-   {
-       //돌아오는 정보 저장
-       _player.gameObject.GetComponent<PlayerInteract>().isInMental = true;
-       _player.gameObject.GetComponent<MentalEnterProcess>().SetCombackEventId("Event_C075");
-       SceneManager.LoadSceneAsync(SceneName);
-   }
+    public void StartAnim(string command)
+    {
+        string objectName = command.Split(':')[0];
+        string animName = command.Split(':')[1];
+        Animator anim = GetAnimator(objectName);
+        anim.SetBool(animName,true);
 
-   public void StartVfx(string SoundResource)
-   {
-       SoundManager.Instance.PlaySFXNoEffect(SoundResource);
-   }
+    }
 
-   public void EndVfx()
-   {
-       SoundManager.Instance.StopAllSFX();
-   }
-   private bool isPlayingFootsteps = false;
-   private async UniTask PlayRandomFootSteps(float duration)
-   {
-       if (footSteps == null || footSteps.Count == 0) return;
-       
-       isPlayingFootsteps = true;
-       float elapsedTime = 0f;
-       float interval = 0.4f;
+    public void EndAnim(string command)
+    {
+        string objectName = command.Split(':')[0];
+        string animName = command.Split(':')[1];
+        Animator anim = GetAnimator(objectName);
+        anim.SetBool(animName, false);
+    }
 
-       while (elapsedTime < duration && isPlayingFootsteps)
-       {
-           int randomIndex = Random.Range(0, footSteps.Count);
-           SoundManager.Instance.PlaySFXNoEffect(footSteps[randomIndex]);
-           
-           await UniTask.Delay(System.TimeSpan.FromSeconds(interval));
-           elapsedTime += interval;
-       }
+    public void SetMaterial(Material material)
+    {
+        _freindMaterial.material = material;
+    }
 
-       isPlayingFootsteps = false;
-   }
+    /// <summary>
+    /// ///////////////////////////////////////////////////
+    /// </summary>
+    [Header("정신세계 이펙트")] [SerializeField] private RawImage _vhsImage;
 
-   public void StartRandomSound(float duration)
-   {
-       PlayRandomFootSteps(duration).Forget();
-   }
+    [SerializeField] private Volume _vhsVolume;
+    [SerializeField] private GameObject _vhsObj;
+    [SerializeField] private float _duration = 5f;
+    private Camera _mainCamera;
+    private UnityEngine.Rendering.Universal.UniversalAdditionalCameraData _cameraData;
 
-   public void StopFootSteps()
-   {
-       isPlayingFootsteps = true;
-   }
+    private async UniTaskVoid StartAutoEffect()
+    {
+        _vhsObj.SetActive(true);
+        _cameraData.renderPostProcessing = true;
 
-   public void CameraON()
-   {
-       _farLoungCam.Priority = 12;
-   }
+        float elapsedTime = 0f;
+        SoundManager.Instance.PlaySFXNoEffect("Soundresource_029");
+        // 5초동안 게이지 증가
+        while (elapsedTime < _duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float value = elapsedTime / _duration;
+
+            Color color = _vhsImage.color;
+            color.a = value;
+            _vhsImage.color = color;
+            _vhsVolume.weight = value;
+
+            await UniTask.Yield();
+        }
+
+        SoundManager.Instance.PlaySFXNoEffect("Soundresource_028");
+        DontDestroyOnLoad(_player.gameObject);
+    }
+
+    public void OnStartMentalProcess()
+    {
+        StartAutoEffect().Forget();
+    }
+
+    public void OnLoadScene(string SceneName)
+    {
+        //돌아오는 정보 저장
+        _player.gameObject.GetComponent<PlayerInteract>().isInMental = true;
+        _player.gameObject.GetComponent<MentalEnterProcess>().SetCombackEventId("Event_C075");
+        SceneManager.LoadSceneAsync(SceneName);
+    }
+
+    public void StartVfx(string SoundResource)
+    {
+        SoundManager.Instance.PlaySFXNoEffect(SoundResource);
+    }
+
+    public void EndVfx()
+    {
+        SoundManager.Instance.StopAllSFX();
+    }
+
+    private bool isPlayingFootsteps = false;
+
+    private async UniTask PlayRandomFootSteps(float duration)
+    {
+        if (footSteps == null || footSteps.Count == 0) return;
+
+        isPlayingFootsteps = true;
+        float elapsedTime = 0f;
+        float interval = 0.4f;
+
+        while (elapsedTime < duration && isPlayingFootsteps)
+        {
+            int randomIndex = Random.Range(0, footSteps.Count);
+            SoundManager.Instance.PlaySFXNoEffect(footSteps[randomIndex]);
+
+            await UniTask.Delay(System.TimeSpan.FromSeconds(interval));
+            elapsedTime += interval;
+        }
+
+        isPlayingFootsteps = false;
+    }
+
+    public void StartRandomSound(float duration)
+    {
+        PlayRandomFootSteps(duration).Forget();
+    }
+
+    public void StopFootSteps()
+    {
+        isPlayingFootsteps = true;
+    }
+
+    public void CameraON()
+    {
+        _farLoungCam.Priority = 12;
+    }
 }
