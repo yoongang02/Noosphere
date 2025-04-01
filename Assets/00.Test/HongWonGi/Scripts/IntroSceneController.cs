0c9a47f23fa8543b06eedaed89ea986b3b12fa87
Hone Won Gi <keyone957@naver.com>
2025-04-01 22:00:00 +0900

    Feat : 타임라인용 대화 함수 추가

--- Assets/00.Test/HongWonGi/Scripts/IntroSceneController.cs
+++ Assets/00.Test/HongWonGi/Scripts/IntroSceneController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Cysharp.Threading.Tasks;
@@ -7,6 +5,9 @@ using DG.Tweening;
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
+using TMPro;
+using Random = UnityEngine.Random;
+
 
 public class IntroSceneController : MonoBehaviour
 {
@@ -14,11 +15,29 @@ public class IntroSceneController : MonoBehaviour
     [SerializeField] private float _fadeDuration = 0.8f;
     [SerializeField] private PlayableDirector _introTimeLine;
     [SerializeField] private VideoPlayer _introVideo;
-    
+
+    [Header("대사관련")] 
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private GameObject _dialogueGroup;
+    [SerializeField] private TextMeshProUGUI _speakerText;
+    [SerializeField] float _letterDelay = 0.05f;
+    private float _currentTextElapsedTime = 0f;
+    private int _currentLetterIndex = 0;
+    private bool isTyping = false;
+    private ContentSizeFitter contentSizeFitter;
+    private RectTransform textRectTransform;
+
+
     private void Start()
     {
+        contentSizeFitter = dialogueText.gameObject.AddComponent<ContentSizeFitter>();
+        contentSizeFitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+        textRectTransform = dialogueText.GetComponent<RectTransform>();
+        textRectTransform.pivot = new Vector2(0, 0.5f);
+
         FadeIn().Forget();
     }
+
     private async UniTask FadeIn()
     {
         _fadeImage.color = new Color(0, 0, 0, 1);
@@ -38,9 +57,95 @@ public class IntroSceneController : MonoBehaviour
     }
 
     #region TimeLineFunc
+
     public void PlayIntroVideo()
     {
         _introVideo.Play();
     }
+
+    public void ShowDialogue(string fullText)
+    {
+        _dialogueGroup.SetActive(true);
+
+        string speaker = "";
+        string text = fullText;
+
+        if (fullText.Contains(":"))
+        {
+            int colonIndex = fullText.IndexOf(":");
+            speaker = fullText.Substring(0, colonIndex).Trim();
+            text = fullText.Substring(colonIndex + 1).Trim();
+        }
+
+        _speakerText.text = speaker;
+        TypeText(text).Forget();
+    }
+
+    public void CloseDialogue()
+    {
+        _dialogueGroup.SetActive(false);
+    }
+
+    private async UniTask TypeText(string text)
+    {
+        isTyping = true;
+        dialogueText.alpha = 0;
+        text = text.Replace("\\n", "\n");
+        dialogueText.text = text;
+        await UniTask.NextFrame();
+
+        float totalWidth = dialogueText.preferredWidth;
+
+        Canvas canvas = dialogueText.transform.GetComponentInParent<Canvas>();
+        RectTransform canvasRect = canvas.GetComponent<RectTransform>();
+        float canvasCenter = canvasRect.rect.width / 2f;
+        float startX = canvasCenter - (totalWidth / 2f);
+        textRectTransform.anchoredPosition = new Vector2(startX, textRectTransform.anchoredPosition.y);
+
+        dialogueText.text = "";
+        dialogueText.alpha = 1;
+
+        // StartDialogueSound(_speakerText.text);
+
+        if (!isTyping)
+        {
+            dialogueText.text = text;
+            return;
+        }
+
+        for (int i = 0; i < text.Length && isTyping; i++)
+        {
+            dialogueText.text += text[i];
+            await UniTask.Delay((int)(_letterDelay * 1000));
+        }
+
+        dialogueText.text = text;
+        isTyping = false;
+        // SoundManager.Instance.StopAllSFX();
+    }
+
+
+    private void StartDialogueSound(string speaker)
+    {
+        switch (speaker)
+        {
+            case "(나)":
+                SoundManager.Instance.PlayLoopingSound("Soundresource_026");
+                break;
+            case "(닥터 로만)":
+                SoundManager.Instance.PlayLoopingSound("Soundresource_027");
+                break;
+            case "(레이)":
+                SoundManager.Instance.PlayLoopingSound("Soundresource_077");
+                break;
+            case "(다프네)":
+                SoundManager.Instance.PlayLoopingSound("Soundresource_076");
+                break;
+            case "":
+                SoundManager.Instance.PlayLoopingSound("Soundresource_075");
+                break;
+        }
+    }
+
     #endregion
 }
