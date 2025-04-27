6b261ee0dd4f4eaa5709637d0c4579e8fab4c96c
YoonKyoungMin <96673951+yoongang02@users.noreply.github.com>
2025-04-27 23:35:04 +0900

    Sound : 문 틈 사운드 연결 및 사운드리소스 114~116 추가

--- Assets/02.Scripts/Player/InitPlayerPos.cs
+++ Assets/02.Scripts/Player/InitPlayerPos.cs
@@ -5,28 +5,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class InitPlayerPos : MonoBehaviour
 {
-    private void Awake()
+    private void Start()
     {
-        Transform player = GameObject.FindWithTag("Player").transform;
-        player.position = gameObject.transform.position;
-        player.rotation = gameObject.transform.rotation;
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
-    // private void Start()
-    // {
-    //     SceneManager.sceneLoaded += OnSceneLoaded;
-    // }
-    //
-    // private void OnDestroy()
-    // {
-    //     SceneManager.sceneLoaded -= OnSceneLoaded;
-    // }
-    //
-    // void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    // {
-    //   Transform player = GameObject.FindWithTag("Player").transform;
-    //   player.position = gameObject.transform.position;
-    //   player.rotation = gameObject.transform.rotation;
-    // }
-
-   
+    
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+    
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+      Transform player = GameObject.FindWithTag("Player").transform;
+      player.position = gameObject.transform.position;
+      player.rotation = gameObject.transform.rotation;
+    }
 }
