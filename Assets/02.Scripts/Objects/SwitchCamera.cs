a1d44b093890bc4d84cd8ade452cb1917b09af65
Hone Won Gi <keyone957@naver.com>
2025-05-29 22:17:43 +0900

    Feat : 스테이지 2 switch카메라 코드 변경.
    - 이벤트 c082 실행됐냐 안됐냐에 따라 바뀌는 카메라 달라짐

--- Assets/02.Scripts/Objects/SwitchCamera.cs
+++ Assets/02.Scripts/Objects/SwitchCamera.cs
@@ -8,29 +8,42 @@ public class SwitchCamera : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera camera1;
     [SerializeField] private bool _onCam1;
-    [SerializeField] private CinemachineVirtualCamera camera2;
+    [SerializeField] private CinemachineFreeLook camera2;
     [SerializeField] private bool _onCam2;
-    
-    
+    [SerializeField] private CinemachineVirtualCamera cutSceneCamera;
+    [SerializeField] private bool _onCam3;
+    [SerializeField] private bool isMainCamTrigger; // true면 메인카메라(camera1), false면 나머지 카메라 조건 활성화
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            if (_onCam1)
-            {
-                camera1.enabled = true;
-                camera2.enabled = false;
-                
-                return;
-            }
+        if (!other.CompareTag("Player"))
+            return;
 
-            if (_onCam2)
-            {
-                camera2.enabled = true;
-                camera1.enabled = false;
-                
-                return;
-            }
+        if (isMainCamTrigger)
+        {
+            ActivateCamera(camera1);
         }
+        else
+        {
+            bool isEventExecuted = DataManager.Instance._events["Event_C082"].isExecuted;
+            if (isEventExecuted)
+                ActivateCamera(cutSceneCamera);
+            else
+                ActivateCamera(camera2);
+        }
+    }
+    
+    private void ActivateCamera(CinemachineVirtualCamera targetCamera)
+    {
+        camera1.enabled = (targetCamera == camera1);
+        cutSceneCamera.enabled = (targetCamera == cutSceneCamera);
+        camera2.enabled = false; // FreeLook 비활성화
+    }
+    
+    private void ActivateCamera(CinemachineFreeLook targetCamera)
+    {
+        camera1.enabled = false;
+        cutSceneCamera.enabled = false;
+        camera2.enabled = (targetCamera == camera2);
     }
 }
