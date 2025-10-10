837adf78068fdbc3d8a69e0b88240f939bbe1e96
Yoon KyoungMin <96673951+yoongang02@users.noreply.github.com>
2025-10-10 22:41:54 +0900

    Fix : 씬 전환 중, Update 문에서 DataManager 접근 Null 오류 해결

--- Assets/02.Scripts/Objects/LoungeKeyController.cs
+++ Assets/02.Scripts/Objects/LoungeKeyController.cs
@@ -12,6 +12,7 @@ public class LoungeKeyController : MonoBehaviour
     private void Update()
     {
         if (DataManager.Instance == null) return;
+        if (!DataManager.Instance._events.ContainsKey("Event_C064")) return;
         if (DataManager.Instance._events["Event_C064"].isExecuted)
         {
             foreach (var room in roomTriggerObjects)
