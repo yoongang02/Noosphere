d8bae13bf3c27588d5091f507dbd4e9149491010
Hone Won Gi <keyone957@naver.com>
2025-11-08 18:20:40 +0900

    Fix : 저장상태에 따른 거울 오브젝트 활,비활성화

--- Assets/02.Scripts/Objects/OnEnabledMirror.cs
+++ Assets/02.Scripts/Objects/OnEnabledMirror.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class OnEnabledMirror : MonoBehaviour
 {
     [SerializeField] private GameObject _mirror;
+
     void Start()
     {
-        if (DataManager.Instance._events["Event_B099"].isExecuted)
+        if (DataManager.Instance._events["Event_B099"].isExecuted&&
+            DataManager.Instance._events["Event_B067"].isExecuted)
+        {
+            _mirror.SetActive(false);
+        }
+        else if (DataManager.Instance._events["Event_B099"].isExecuted )
         {
             _mirror.SetActive(true);
         }
@@ -16,6 +22,5 @@ public class OnEnabledMirror : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
