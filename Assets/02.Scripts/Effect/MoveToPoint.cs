40ac667e9f4d58b18a8e460d45575489599afc66
Hone Won Gi <keyone957@naver.com>
2025-11-26 20:20:13 +0900

    Fix : 타임라인 실행시 위치이동
    - 엘리스 대사후 뒤에 타임라인 실행하는 과정에서 위치동기화

--- Assets/02.Scripts/Effect/MoveToPoint.cs
+++ Assets/02.Scripts/Effect/MoveToPoint.cs
@@ -8,6 +8,8 @@ public class MoveToPoint : MonoBehaviour
 {
     [SerializeField] private Transform _point;
     [SerializeField] private float _walkDuration;
+    [SerializeField] private float _rotate;
+    [SerializeField] private bool _isRotate;
     private Animator _playerAnim;
     private GameObject _player;
 
@@ -24,8 +26,38 @@ public class MoveToPoint : MonoBehaviour
         UIManager.Instance.LockPlayer();
         UIManager.Instance.inventoryIcon.SetActive(false);
         _playerAnim.SetBool("IsWalk", true);
+        if (_isRotate)
+        {
+            MoveWithRotate();
+        }
+        else
+        {
+            NormalMove();
+        }
+
+    }
+
+    private void NormalMove()
+    {
         _player.transform.LookAt(_point.position);
         _player.transform.DOMove(_point.transform.position, _walkDuration).OnComplete(() =>
+        {
+            PlayerController.Instance.canMove = true;
+            _playerAnim.SetBool("IsWalk", false);
+            EffectManager.Instance?.OnEffectEnd.Invoke();
+            gameObject.SetActive(false);
+        });
+    }
+
+    private void MoveWithRotate()
+    {
+        Sequence seq = DOTween.Sequence();
+
+        seq.Join(_player.transform.DOMove(_point.position, _walkDuration).SetEase(Ease.Linear));
+
+        seq.Join(_player.transform.DORotate(new Vector3(0, _rotate, 0), _walkDuration).SetEase(Ease.Linear));
+
+        seq.OnComplete(() =>
         {
             PlayerController.Instance.canMove = true;
             _playerAnim.SetBool("IsWalk", false);
