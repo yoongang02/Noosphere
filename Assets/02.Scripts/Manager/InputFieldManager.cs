66aa50f5394e586ce163677ad7cee07b6d36c859
Hone Won Gi <keyone957@naver.com>
2025-12-08 21:16:03 +0900

    Fix : 박사 이름 맞추기 다국어적용

--- Assets/02.Scripts/Manager/InputFieldManager.cs
+++ Assets/02.Scripts/Manager/InputFieldManager.cs
@@ -4,16 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
 using TMPro;
+using UnityEngine.Localization.Settings;
 
 public class InputFieldManager : UIBase
 {
-    [Header("Input Field 변수")]
-    public bool isAnswer = false;
+    [Header("Input Field 변수")] public bool isAnswer = false;
     [SerializeField] private GameObject _inputFieldUI;
     [SerializeField] private TMP_InputField _inputText;
     private string _currentAnswer;
     private string _currentID;
     
+
 
     private void Start()
     {
@@ -27,6 +28,7 @@ public class InputFieldManager : UIBase
         EscapeUI.Instance.Active();
     }
     
+
     public override void OnClose()
     {
         base.OnClose();
@@ -37,7 +39,7 @@ public class InputFieldManager : UIBase
         
         QuizManager.Instance.OnQuizEnd?.Invoke();
     }
-    
+
     /// <summary>
     // inputfield 활성화 되고 inputfield 활성화
     /// </summary>
@@ -46,29 +48,37 @@ public class InputFieldManager : UIBase
     {
         _currentID = id;
         isAnswer = false;
-        
+
         if (!DataManager.Instance._quiz.TryGetValue(id, out QuizStructure structure))
         {
             Debug.LogWarning($"ID {id}에 해당하는 InputFieldStructure를 찾을 수 없습니다.");
             return;
         }
-        _currentAnswer = structure.correctAnswer;
+
+        // _currentAnswer = structure.correctAnswer;
+        _currentAnswer = AnswerName();
         InitInputField();
-        
+
         //input 입력창 열기
         UIManager.Instance.OpenUI(UIManager.Instance.inputFieldUI);
-        
+
         //input 입력창 활성화
         _inputText.Select();
         _inputText.ActivateInputField();
     }
+
     private void OnInputFieldEndEdit(string value)
     {
         if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
         {
             string formattedValue = value.Replace(" ", "");
             string formattedAnswer = _currentAnswer.Replace(" ", "");
-
+            string local = LocalizationSettings.SelectedLocale.ToString();
+            if (local == "English (en)")
+            {
+                formattedValue = formattedValue.ToLower();
+                formattedAnswer = formattedAnswer.ToLower();
+            }
             if (formattedValue == formattedAnswer)
             {
                 Debug.Log("#input 정답 맞춤");
@@ -81,6 +91,7 @@ public class InputFieldManager : UIBase
                 isAnswer = false;
                 DataManager.Instance._quiz[_currentID].isSolved = false;
             }
+
             //Input Field UI 종료
             UIManager.Instance.CloseTopUI();
         }
@@ -89,5 +100,24 @@ public class InputFieldManager : UIBase
     private void InitInputField()
     {
         _inputText.text = "";
+    }
+
+    private string AnswerName()
+    {
+        string local = LocalizationSettings.SelectedLocale.ToString();
+        string answer="";
+        switch (local)
+        {
+            case"English (en)":
+                answer= "Rachel Ko";
+                break;
+            case "Korean (ko)":
+                answer="레이첼 코";
+                break;
+            case "Chinese (Simplified)(zh)" :
+                answer="雷切尔科";
+                break;
+        }
+        return answer;
     }
 }
