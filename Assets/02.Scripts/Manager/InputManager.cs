14ca0a7aaab4f9796ed99de6fba08be6d23c86c9
YoonKyoungMin <96673951+yoongang02@users.noreply.github.com>
2025-12-19 23:24:57 +0900

    Fix : QA 68번 오류 해결
    Some objects were not cleaned up when closing the scene. (Did you spawn new GameObjects from OnDestroy?)
    The following scene GameObjects were found:
    InputManager
    
    이 오류 메세지가 발생함.
    InputManager.cs를 기존에 사용하던 Singleton 스크립트를 상속받지 않고, 직접 스크립트 내에 싱글톤 코드를 작성해 줌.

--- Assets/02.Scripts/Manager/InputManager.cs
+++ Assets/02.Scripts/Manager/InputManager.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using UnityEngine;
 
-public class InputManager : Singleton<InputManager>
+public class InputManager : MonoBehaviour
 {
+    public static InputManager Instance { get; private set; }
     public Action moveAction = null;
     public Action selectBtnAction = null;
     public Action exitBtnAction = null;
 
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     public void OnUpdate()
     {
         // if(Input.anyKey==false)
